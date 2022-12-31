752c0fc17d79f6e039950487be882fbf22eddbc7
Bianca Emi <bemi.brando@outlook.com>
2022-12-31 01:27:06 -0300

    Update README and update Global settings

--- global_settings_default.py
+++ global_settings_default.py
@@ -1,7 +1,8 @@
 #region ROGER SETUP
 __b2droid_icon__ = './src/assets/icons/b1droid2.ico'
 __screen_size__ = '1000x800'
-__data_base__ = 'COMPLETE_PAHT_STORAGE_YOUR_DATABASE'
+#__data_base__ = 'COMPLETE_PAHT_STORAGE_YOUR_DATABASE'
+__data_base__ = './database/user/default/'
 #endregion
 
 #region SETTING PATH
@@ -9,6 +10,8 @@ __expenses_path__ = __data_base__ + 'expenses/'
 __receipt_history__ = __data_base__ + 'exepnses/receipt_history.xlsx'
 __expend_backup__ = __data_base__ + 'expenses/backup/'
 
-__expenses__ = 'COMPLETE_PAHT/FILE_NAME.xlsx'
-__expenses_page__ = '''SHEET NAME'''
+#__expenses__ = 'COMPLETE_PAHT/FILE_NAME.xlsx'
+__expenses__ = __expenses_path__ + 'HouseCare.xlsx'
+#__expenses_page__ = '''SHEET NAME'''
+__expenses_page__ = '''Market Expenses'''
 #endregion
